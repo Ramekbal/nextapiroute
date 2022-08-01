952fe2022c68ed041dcce60edd9226bd136041e4
bala <ramekbal159@gmail.com>
2022-08-01 11:32:37 +0530

    poset request

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,5 +1,16 @@
 import { comment } from "../../../data/comments";
 
 export default function handler(req, res){
-    res.status(200).json(comment);
+    if(req.method=="GET"){
+        res.status(200).json(comment);
+    }
+    else if(req.method=="POST"){
+        const commentsss = req.body;
+        const newComment ={
+            id:Date.now(),
+            text:commentsss
+        }
+        comment.push(newComment);
+        res.status(201).json(newComment)
+    }
 }
--- pages/comment/index.js
+++ pages/comment/index.js
@@ -2,14 +2,29 @@ import React, { Fragment, useState } from 'react';
 
 export default function Index() {
     const [commentData, setCommentData]=useState([])
+    const [comment, setComment]=useState('')
 
 const  fectCommentData  = async ()=>{
         const data = await fetch("api/comments").then((comment)=>comment.json());
         setCommentData(data)
 }
+const sendData = async() =>{
+    const response = await fetch("api/comments", {
+      method:"POST",
+      body:JSON.stringify(comment),
+      headers:{
+        'Content-Type':"Application/json",
+      }
+    })
+    const data = response.json();
+    console.log(data);
+}
 
   return (
     <div>
+        <input type="text" value={comment} onChange={(e)=>setComment(e.target.value)}/>
+        {comment}<br/>
+        <button onClick={sendData}>Submit data</button><br/>
         <button onClick={fectCommentData}>Load data</button>
       {commentData.map((data)=>{
         return(
