f3ca74fac27c8ac33ca6b96e26207fb51d99ae42
bala <ramekbal159@gmail.com>
2022-08-01 17:42:39 +0530

    head component for seo

--- pages/comment/about.js
+++ pages/comment/about.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Footer from '../../components/Footer';
 import Style from "./about.module.scss";
+import Head from 'next/head';
 
 export default function About() {
   return (
-    <div>
+    <>
+    <Head>
+      <title>About </title>
+      <meta name='description' content='About page seo'></meta>
+    </Head>
       <h2 className={Style.h2Style}>This is a about page</h2>
-    </div>
+    </>
   )
 }
 
