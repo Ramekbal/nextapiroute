79d5c20dc5084e6d5a3faf42fa31b4591152e116
bala <ramekbal159@gmail.com>
2022-08-01 16:28:05 +0530

    scss module

---
+++ pages/comment/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Style from "./about.module.scss";
+
+export default function About() {
+  return (
+    <div>
+      <h2 className={Style.h2Style}>This is a about page</h2>
+    </div>
+  )
+}
