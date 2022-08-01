935d2fced97f021a2b85af13044801d1f6561613
bala <ramekbal159@gmail.com>
2022-08-01 16:03:18 +0530

    component based css means css module

---
+++ pages/comment/contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Styles  from "./contact.module.css";
+
+export default function contact() {
+  return (
+    <div>
+      <h1 className={Styles.h1Style}>Hii this side bala</h1>
+    </div>
+  )
+}
