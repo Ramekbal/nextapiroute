0af77a86c8bb1c91040c795b8a0db4f22204d5d1
bala <ramekbal159@gmail.com>
2022-08-01 15:35:32 +0530

    add dynamic api route and glbal css

---
+++ pages/comment/[commentsid].js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { comment } from '../../data/comments'
+
+export default function CommentDetails({findElement}) {
+  return (
+    <div>
+      <h1>{findElement.text}</h1>
+    </div>
+  )
+}
+
+export async function getStaticPaths(context){
+
+    return {
+        paths:[
+           { params:{commentsid:"1"}},
+           { params:{commentsid:"2"}},
+            {params:{commentsid:"3"}}
+        ],
+        fallback:false
+    }
+}
+
+export async function getStaticProps(context){
+    const {params} = context;
+    const {commentsid} = params;
+    const findElement= comment.find((commentss)=>commentss.id==commentsid);
+
+    return {
+        props:{
+            findElement:findElement
+        }
+    }
+
+}
+
